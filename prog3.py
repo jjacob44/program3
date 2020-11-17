2cba8141fb6ae6b735633cfacad67a0b804f872d
Jordan Jacobson <jordanmaxj@gmail.com>
2020-11-17 12:21:47 -0500

    first version of main menu created

---
+++ prog3.py
@@ -0,0 +1,6 @@
+menu = "Please select an option below:\n    1 - Lower sales in March 2020 than March 2019\n    2 - Higher sales in June 2020 than June 2019\n    3 - International average sports equipment market size\n    4 - Sporting goods retail sales Aprils of 2017-2020\n    5 - Average sporting goods sales in April before 2020\n    6 - Lowest grossing month for sporting goods sales between 2017-2020\n    7 - Sporting goods retail sales Junes of 2017-2020\n    8 - Average sporting goods sales in June before 2020\n    9 - Highest grossing month for sporting goods sales between 2017-2020\n    10 - Outdoor sporting goods sales stats 2017-2020\n    11 - Outdoor sporting goods sales growith 2019-2020"
+
+
+def main():
+    print(menu)
+main()
